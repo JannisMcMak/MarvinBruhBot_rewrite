9f6c575ea43db77176f38239f81a6c199360e277
JannisMcMak <lehner.jannis@web.de>
2021-11-02 21:55:43 +0100

    clear cache functionality

--- bot.py
+++ bot.py
@@ -1,11 +1,8 @@
 from discord.ext import commands
 import discord
 import os
+import shutil
 from dotenv import load_dotenv
-
-
-os.system('chmod +777 ./ffmpeg')
-
 
 intents = discord.Intents.default()
 intents.members = True
@@ -42,6 +39,21 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
+
+def clear_cache():
+  print('Clearing cache...')
+  folder = 'cache'
+  for filename in os.listdir(folder):
+    file_path = os.path.join(folder, filename)
+    try:
+      if os.path.isfile(file_path) or os.path.islink(file_path):
+        os.unlink(file_path)
+      elif os.path.isdir(file_path):
+        shutil.rmtree(file_path)
+    except Exception as e:
+      print('Error clearing cache: {}'.format(e))
+
+clear_cache()
 
 bot.load_extension("cogs.admin")
 bot.load_extension("cogs.bitch")
