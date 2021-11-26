68990a7c2295f4c0b3d2d03cdd0c3c2619797e7d
JannisMcMak <lehner.jannis@web.de>
2021-11-26 01:25:52 +0100

    add hourly notification task

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -19,7 +19,44 @@ class Tasks(commands.Cog):
         self.bot = bot
         self.rl_tournament_time = os.environ["RL_TOURNAMENT_TIME"]
 
+        if os.environ["HOURLY_TIME_NOTIFICATIONS"] == "yes":
+            log.debug("Hourly time notifications enabled!")
+            self.time_loop.start()
 
+
+    @commands.command(help="Toggle hourly time notifications")
+    async def uhr(self, ctx):
+        if self.time_loop.is_running() and not self.time_loop.is_being_cancelled():
+            log.info("Hourly notifications enabled!")
+            self.time_loop.start()
+            await ctx.send("Hourly notifications enabled!")
+        else:
+            log.info("Hourly notifications disabled!")
+            self.time_loop.cancel()
+            await ctx.send("Hourly notifications disabled!")
+
+    @tasks.loop(seconds=60)
+    async def time_loop(self):
+        minutes = (datetime.now(tz=pytz.timezone(os.environ["TZ"]))).strftime('%M')
+        if "00" in minutes:
+            hour = (datetime.now(tz=pytz.timezone(os.environ["TZ"]))).strftime('%I')
+
+            text = await util.utilities.get_time_notification(int(hour))
+
+            guild = None
+            for g in self.bot.guilds:
+                if g.id == int(os.environ["MAIN_GUILD"]):
+                    guild = g
+
+            channel = None
+            if guild is not None:
+                for c in guild.voice_channels:
+                    if channel is None or len(channel.members) < len(c.members):
+                        channel = c
+            
+            if channel is not None:
+                filename = await tts.write_mp3_twitch(text, True)
+                await tts.play_in_channel(filename, channel)
 
 
     @commands.command(help="Rocket League tournament notifications")
--- util/utilities.py
+++ util/utilities.py
@@ -12,6 +12,15 @@ async def get_gedicht(i):
     
     return data.get(str(i), 'Kein Gedicht gefunden...')
 
+async def get_time_notification(i):
+  with open('hidden/time_notifications.json') as json_file:
+    data = json.load(json_file)
+
+    if i == 0:
+      i = random.randint(1, len(data))
+    
+    return data.get(str(i), "I don't no man")
+
 
 
 def get_command_name(message: discord.Message):
